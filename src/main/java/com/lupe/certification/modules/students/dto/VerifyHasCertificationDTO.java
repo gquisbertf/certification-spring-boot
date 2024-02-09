caefa5d0ec7a6edec0989be5e9510ca432f0426f
gquisbert <gquisbertfernandez@gmail.com>
2024-02-08 23:13:20 -0400

    Initial commit

---
+++ src/main/java/com/lupe/certification/modules/students/dto/VerifyHasCertificationDTO.java
@@ -0,0 +1,14 @@
+package com.lupe.certification.modules.students.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VerifyHasCertificationDTO {
+
+    private String email;
+    private String technology;
+}
---
+++ src/main/java/com/lupe/certification/modules/students/useCases/VerifyIfHasCertificationUseCase.java
@@ -0,0 +1,23 @@
+package com.lupe.certification.modules.students.useCases;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lupe.certification.modules.students.dto.VerifyHasCertificationDTO;
+import com.lupe.certification.modules.students.repositories.CertificationStudentRepository;
+
+@Service
+public class VerifyIfHasCertificationUseCase {
+
+    @Autowired
+    private CertificationStudentRepository certificationStudentRepository;
+
+    public boolean execute(VerifyHasCertificationDTO dto) {
+        var result = this.certificationStudentRepository.findByStudentEmailAndTechnology(dto.getEmail(),
+                dto.getTechnology());
+        if (!result.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+}
